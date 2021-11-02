c954ec5aa1534a9706e0ae0b13d5d4b6f05ed89a
Alexander Meissner <alexander.meissner@rwth-aachen.de>
2021-11-02 13:23:33 +0100

    window creation

--- Vulkan/_main.cpp
+++ Vulkan/_main.cpp
@@ -1,16 +1,41 @@
 #include <Windows.h>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 class vulkan_application
 {
 public:
     void run()
     {
-        initialize();
-        loop();
-        cleanup();
+        if (initialize_window(800, 600))
+        {
+            initialize();
+            loop();
+            cleanup();
+        }        
     }
 
 private:
+    bool initialize_window(const uint32_t width, const uint32_t height)
+    {
+        if (glfwInit() != GL_TRUE)
+        {
+            return false;
+        }
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(width, height, "Vulkan", nullptr, nullptr);
+
+        if (!window)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
     void initialize()
     {
 
@@ -18,13 +43,20 @@ private:
 
     void loop()
     {
-
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
     }
 
     void cleanup()
     {
-
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
+
+private:
+    GLFWwindow* window = nullptr;
 };
 
 int main()
