24a796940fec4ff0bfb1f8e6fdd4471fa1230f84
Alexander Meissner <alexander.meissner@rwth-aachen.de>
2021-11-02 13:12:48 +0100

    vulkan application class

--- Vulkan/_main.cpp
+++ Vulkan/_main.cpp
@@ -1,4 +1,36 @@
+#include <Windows.h>
+
+class vulkan_application
+{
+public:
+    void run()
+    {
+        initialize();
+        loop();
+        cleanup();
+    }
+
+private:
+    void initialize()
+    {
+
+    }
+
+    void loop()
+    {
+
+    }
+
+    void cleanup()
+    {
+
+    }
+};
+
 int main()
 {
-    return 0;
+    vulkan_application app;
+    app.run();
+
+    return EXIT_SUCCESS;
 }
